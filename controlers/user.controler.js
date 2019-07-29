b331e84ab914fe3d5d1ac743f40ba431b6fb9e0d
GarikSaghumyan <saghumyangarik@gmail.com>
2019-07-29 14:42:34 +0400

    Added all APIs an validations and fixed bugs

--- controlers/user.controler.js
+++ controlers/user.controler.js
@@ -4,17 +4,26 @@ const nodemailer = require('nodemailer');
 const jwt = require("jsonwebtoken");
 const key = { tokenKey: "djghhhhuuwiwuewieuwieuriwu" };
 const uuidv4 = require('uuid/v4');
+const log4js = require('log4js');
+const dotenv = require('dotenv');
+const { validationResult } = require('express-validator');
+
+dotenv.config();
+const logger = log4js.getLogger();
+logger.level = "debug";
 let transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'authorisation.app@gmail.com',
-        pass: 'Hunan1996'
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
     }
 });
 
-let resetPassword = async function(req, res) {
+let resetPassword = async function (req, res) {
     const url = 'http://localhost:3000';
-    if (!req.body) return res.sendStatus(400);
+    if (!req.body) {
+        return res.sendStatus(400);
+    }
     const email = req.body.email;
     const newPass = uuidv4();
     const cryptPass = bcrypt.hashSync(newPass, 10);
@@ -22,91 +31,124 @@ let resetPassword = async function(req, res) {
         from: 'authorisation.app@gmail.com',
         to: email,
         subject: 'Forgot password',
-        html: `Your new password is:${newPass}<br>  Please click this to login with your new password: <a href="${url}">${url}</a>`,
+        html: `Your new password is:${newPass}<br>  Please click this to login with your new password: <a href="${url}">Go to login page</a>`,
     };
     try {
         await transporter.sendMail(mailOptions);
+        logger.info(`Transport the message: ${mailOptions}`)
         const info = await User.findOne({ "email": email });
         if (!info) {
+            logger.error(`Not found user to reset password, email: ${email}`)
             return res.status(404).json({ message: "User not found" });
-        } else  {
-            const data = await User.updateOne({ "email": info.email }, { "password": cryptPass });
+        }
+        const data = await User.updateOne({ "email": info.email }, { "password": cryptPass });
+        logger.info(`User to reset password: ${info}`);
+        if (data) {
+            logger.info(`User password updated(reset): ${data}`);
             return res.status(200).json({
                 message: "Successfully reseted password"
             });
-        } 
+        }
+        logger.error(`User not found to reset password`);
+        return res.status(404).json({ message: "User not found" });
     } catch (error) {
+        logger.error(`Reset password error: ${error}`);
         return res.status(204).json(error);
     }
 }
+
 let changePassword = async function (req, res) {
     if (req.body) {
-        const email = req.body.email;
+        logger.info(`Change password requset body is exist: ${req.body}`);
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).jsonp(errors.array());
+        }
         const password = req.body.password;
         const newPass = bcrypt.hashSync(req.body.newPassword, 10);
         const token = req.headers.token;
         if (token && password && newPass) {
+            logger.info(`Change password token, old and new passwords are exist:
+                 ${token} ${password} ${newPass}`);
             try {
-                const info = await User.findOne({ "email": email });
+                const info = await User.findOne({ "token": token });
                 if (!info) {
+                    logger.error('Change password error, user not found');
                     return res.status(404).json({ message: "User not found" });
                 } else if (token == info.token && bcrypt.compareSync(password, info.password)) {
-                    const data = await User.updateOne({ "email": info.email }, { "password": newPass });
+                    const data = await User.updateOne({ "token": info.token }, { "password": newPass });
+                    logger.info('Succesfully changed password');
                     return res.status(200).json({
                         message: "Successfully changed password"
                     });
-                } else {
-                    return res.status(302).json({
-                        message: "Cant Do this"
-                    });
                 }
             } catch (err) {
+                logger.error(`Change password error: ${err}`);
                 return res.status(204).json(err);
             }
         } else {
+            logger.error('Change password, not valid email or password');
             return res.status(403).json({ message: "Not valid email or password" });
         }
     } else {
+        logger.error(`Change password request body is empty`);
         return res.status(202).json({ message: "Empty data!" })
     }
 }
+
 let loginUser = async function (req, res) {
     if (req.body) {
+        logger.info(`Login user requset body is exist: ${req.body}`);
         const email = req.body.email;
         const password = req.body.password
         if (email && password) {
+            logger.info(`Login user email & password is exist: ${email} : ${password}`);
             try {
                 const info = await User.findOne({
                     "email": email
                 });
 
                 if (!info) {
+                    logger.error(`Login user not found, email: ${email}`);
                     return res.status(404).json({
                         message: "User not found"
                     });
-                } else {
-                    if (bcrypt.compareSync(password, info.password)) {
-                        const token = jwt.sign({ userId: info._id }, key.tokenKey, { expiresIn: "2h" });
-                        try {
-                            const user = await User.updateOne({ _id: info._id }, {
-                                $set: { token: token }
-                            })
-                            res.status(200).json(info);
-                        } catch (error) {
-                            return res.status(400).json(error);
-                        }
-
-                    }
                 }
+                logger.info(`Login user founded: ${email}`);
+                if (bcrypt.compareSync(password, info.password)) {
+                    if (!info.isVerified) {
+                        logger.error(`Account is not verified : ${info}`);
+                        return res.status(403).json({ message: 'Account is not verified' });
+                    }
+                    const token = jwt.sign({ userId: info._id }, key.tokenKey, { expiresIn: "2h" });
+                    try {
+                        const user = await User.updateOne({ _id: info._id }, {
+                            $set: { token: token }
+                        })
+                        info.token = token;
+                        logger.info(`User succesfully sighn in: ${info}`);
+                        res.status(200).json(info);
+                    } catch (error) {
+                        logger.error(`Password is not valid: ${error}`);
+                        return res.status(400).json(error);
+                    }
+                } else {
+                    logger.error(`Error to login user`);
+                    return res.status(401).json({ message: 'Password is not valid' });
+                }
+
             } catch (err) {
+                logger.error(`Error to login user: ${err}`);
                 return res.status(204).json(err);
             }
         } else {
+            logger.error(`Not valid user email or password to login, email: ${email}, password : ${password}`);
             return res.status(403).json({
                 message: "Not valid email or password"
             });
         }
     } else {
+        logger.error(`Login request body is empty`);
         return res.status(202).json({
             message: "Empty data!"
         })
@@ -117,13 +159,32 @@ let emailConfirmation = async function (req, res) {
     try {
         const user = await User.updateOne({ confirmToken: req.params.token },
             { $set: { isVerified: true, confirmToken: '' } });
-        res.send(user);
-    } catch (e) {
-        res.send('error');
+        logger.info(`Email confirmation, user : ${user}`);
+        res.status(200).json(user);
+    } catch (err) {
+        logger.error(`Email confirmation error: ${err}`);
+        res.status(404).json({ message: 'Email confirmation failed' });
     }
 }
+
 let userPostFunction = async function (req, res) {
-    if (!req.body || !req.body.password) return res.sendStatus(400);
+    if (!req.body) {
+        logger.error(`User registration request body is not exist`);
+        return res.sendStatus(400);
+    }
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).jsonp(errors.array());
+    }
+
+    try {
+        const emailUser = await User.findOne({ "email": req.body.email });
+        if (emailUser) {
+            return res.status(409).json({ message: 'Email is already exists' })
+        }
+    } catch (error) {
+        return res.status(404).json({ error: error })
+    }
     const userName = req.body.name;
     const userEmail = req.body.email;
     const hash = bcrypt.hashSync(req.body.password, 10);
@@ -132,12 +193,14 @@ let userPostFunction = async function (req, res) {
     const token = '';
     const url = `http://localhost:8080/signup/confirmation/${confirmToken}`;
 
+
     let mailOptions = {
         from: 'authorisation.app@gmail.com',
         to: '',
         subject: 'Confirm Email',
-        html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`,
+        html: `Please click this email to confirm your email: <a href="${url}">Confirm your account</a>`,
     };
+
     const user = new User({
         id: userId,
         name: userName,
@@ -147,27 +210,29 @@ let userPostFunction = async function (req, res) {
         token: token
     });
 
+    logger.info(`New user for registration: ${user}`);
     try {
         const result = await user.save();
+        logger.info(`Succesfully registrated user: ${user}`);
         res.send(result);
-
     } catch (error) {
+        logger.error(`User ragistration error : ${error}`);
         res.status(422).json({
             message: error.message
         });
-
     }
     mailOptions.to = req.body.email;
     try {
+        logger.info(`Send mail to verify account, mailOptions: ${mailOptions}`);
         await transporter.sendMail(mailOptions);
     } catch (error) {
-        console.log(error);
+        logger.error(`Error to send Mail: ${error}`);
+        res.status(403).json({ message: 'Email sendin failed' });
     }
+
 }
 module.exports.userPostFunction = userPostFunction;
 module.exports.emailConfirmation = emailConfirmation;
 module.exports.loginUser = loginUser;
 module.exports.changePassword = changePassword;
 module.exports.resetPassword = resetPassword;
-
-
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controlers/user.controler')
+const { check } = require('express-validator');
 
 router.route('/signup')
-    .post(controller.userPostFunction);
+    .post([check('name').matches(/^[A-Z]{1}[a-z]{1,}$/).withMessage('Names first simbol must upper'),
+    check('email').isEmail(),
+    check('password').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/).withMessage('Password must be contain at least one uppercase character, one lowercase character and one ')],
+        controller.userPostFunction);
 router.route('/signup/confirmation/:token')
     .get(controller.emailConfirmation);
 router.route('/signin')
     .post(controller.loginUser);
 router.route('/change')
-    .post(controller.changePassword);
+    .post([check('password').matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/).withMessage('Password must be contain at least one uppercase character, one lowercase character and one ')],
+        controller.changePassword);
 router.route('/resetpass')
     .post(controller.resetPassword)
 module.exports = router;
